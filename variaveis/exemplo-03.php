61450a8426dd737dea7f0a83b07c8e851053afe4
Anderson Wladis <anderson.awbf@gmail.com>
2020-02-20 17:33:52 -0400

    atualizado

--- variaveis/exemplo-03.php
+++ variaveis/exemplo-03.php
@@ -11,25 +11,15 @@ $frutas = array("abacaxi","laranja","manga"); //array() é um construtor da ling
 
 //echo $frutas[2]; 
 
-$nascimento = new DateTime();
+//$nascimento = new DateTime();
 
 //var_dump($nascimento);
 ////////////////////////////////////////////
-<<<<<<< HEAD
-$arquivo = fopen("exemplo-03.php", "r"); //fopen — Abre um arquivo ou URL
-=======
-$arquivo = fopen("exemplo-03.php", "r");//fopen — Abre um arquivo ou URL
->>>>>>> 4164ea0d22f136d66f650833f28d692c34c5229b
+//$arquivo = fopen("exemplo-03.php", "r"); //fopen — Abre um arquivo ou URL
 
 //var_dump($arquivo);
 
 $nulo = NULL;
 $vazio = "";
-
-<<<<<<< HEAD
-echo $nome." ".$email."<br/>"."<b>nascimento</b>"." ".$ano;
-
+echo $nome;
 ?>
-=======
-?>
->>>>>>> 4164ea0d22f136d66f650833f28d692c34c5229b
