102dfe7dab8134414d6c6433514157d70c664511
Anderson Wladis <anderson.awbf@gmail.com>
2020-02-20 17:59:53 -0400

    atualizado

---
+++ concatenação/concatenacao-01.php
@@ -0,0 +1,14 @@
+<?php
+//Declaração de Variáveis
+$nome = "Anderson";
+$sobrenome = "Wladis Borges Ferreira";
+$email = "anderson.awbf@gmail.com";
+$nascimento = 1987;
+$pais = "Brasil";
+
+//Utilizando a 1º forma
+$nomeCompleto = "<b>".$nome." ".$sobrenome."</b>"."<br/>";
+echo $nomeCompleto,$email."<br/>".$nascimento."<br/>".$pais;
+
+
+?>
