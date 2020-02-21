d351ffbe3e951fa75896d5ca030c4c29b2a327ce
Anderson Wladis <anderson.awbf@gmail.com>
2020-02-21 09:21:14 -0400

    Concatenação - versão 1

--- concatenação/concatenacao-01.php
+++ concatenação/concatenacao-01.php
@@ -8,7 +8,28 @@ $pais = "Brasil";
 
 //Utilizando a 1º forma
 $nomeCompleto = "<b>".$nome." ".$sobrenome."</b>"."<br/>";
-echo $nomeCompleto,$email."<br/>".$nascimento."<br/>".$pais;
+echo $nomeCompleto,$email."<br/>".$nascimento."<br/>".$pais."<br/><br/>";
 
+//Utilizando a 2º forma - atribuição
+$nomeCompleto = "<b>"."$nome "."</b>";
+$nomeCompleto .= "<b>"."$sobrenome"."</b>"."<br/>";
+$nomeCompleto .= "$email"."<br/>";
+$nomeCompleto .= "$nascimento"."<br/>";
+$nomeCompleto .= "$pais"."<br/><br/>";
+
+echo $nomeCompleto;
+
+//Utilizado a 3º forma - utilizando chaves para concatenar
+$nomeCompleto = $nome;
+$nomeCompleto .= $sobrenome;
+$nomeCompleto .= $email;
+$nomeCompleto .= $nascimento;
+$nomeCompleto .= $pais;
+
+echo "Meu nome é <b>{$nome} {$sobrenome}</b>, meu e-mail:{$email}<br/>Nascido no ano:{$nascimento} no {$pais}."."<br/><br/>";
+
+//Utilizando a 4º forma - apenas aspas duplas
+
+echo "Meu nome é <b>$nome $sobrenome</b>, meu e-mail:$email<br/>Nascido no ano:$nascimento no $pais."."<br/><br/>"; 
 
 ?>
