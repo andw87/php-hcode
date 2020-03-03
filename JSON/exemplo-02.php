450de8cfa630940020fa60df0884ec9380a7df46
Anderson Wladis <anderson.awbf@gmail.com>
2020-03-03 17:49:41 -0400

    Atualizado 03/03/2020

--- JSON/exemplo-02.php
+++ JSON/exemplo-02.php
@@ -1,5 +1,7 @@
 <?php
 //https://www.php.net/manual/pt_BR/function.json-decode.php
+//https://www.w3schools.com/php/php_ref_json.asp
+
 $json = '[{"nome":"Anderson","idade":"33"},{"nome":"Edilene","idade":"38"}]';
 
 //json_decode — Decodifica uma string JSON
--- introdução/HelloWorld.php
+++ introdução/HelloWorld.php
@@ -1,5 +1,5 @@
 <?php
-
+//https://pt.wikipedia.org/wiki/Programa_Ol%C3%A1_Mundo
 echo "Meu primeiro PHP! <strong>Hello World</strong>"
 
 ?>
