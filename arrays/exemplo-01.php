dee223958e1ef42142ade80a83a71af772186586
Anderson <anderson.awbf@gmail.com>
2020-03-09 17:12:10 -0400

    atualizado 09/03/2020

--- arrays/exemplo-01.php
+++ arrays/exemplo-01.php
@@ -4,15 +4,11 @@
 /*
 Um array no PHP é na verdade um mapa ordenado. 
 Um mapa é um tipo que relaciona valores a chaves.
-Este tipo é otimizado para várias usos diferentes:
+Este tipo é otimizado para vários usos diferentes:
 ele pode ser tratado como um array, uma lista (vetor),
 hashtable (que é uma implementação de mapa), dicionário,
 coleção, pilha, fila e provavelmente mais. Assim como existe a possibilidade 
 dos valores do array serem outros arrays, árvores e arrays multidimensionais.
-
-A explicação dessas estruturas está além do escopo desse manual, 
-mas pelo menos um exemplo é dado para cada uma delas. Para mais informações, 
-busque a considerável literatura que existe sobre esse extenso tópico.
 */
 $frutas = array("laranja", "abacaxi", "melancia");//Array unidimensional: Também conhecido como vetor, é uma tabela de apenas uma coluna e varias linhas
 
